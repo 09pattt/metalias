d53cb3d38535e18133286468f67cf72f8a27d9ee
PorAust <09pattt@gmail.com>
2025-10-27 23:18:39 +0700

    file_utils developed-231827102025

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -1,23 +1,53 @@
 import pathlib
 import os
+import shutil
 from pathlib import Path
 
-def cwd(): # Current working directory
-    return Path.cwd()
+def appdir():
+    return Path(__file__).resolve().parent.parent.parent
 
-def touch(path: str, exist_ok: bool = False):
-    file = Path(str(cwd()))
-    file = file / path
-    file.resolve()
+def join(*args, resolve: bool = True):
+    path = Path("")
+    for i in range(len(args)):
+        tail = Path(args[i])
+        path = path / tail
+    if resolve: path = path.resolve()
+    return path
+
+def mkfile(path: str, exist_ok: bool = False):
+    file = Path(path).resolve()
     file.touch(exist_ok=exist_ok)
 
-def mkdir(path: str, exist_ok: bool = False, parents: bool = False):
-    file = Path(str(cwd()))
-    file = file / path
-    file.resolve()
+def rmfile(path: str):
+    f = Path(path)
+    if f.exists():
+        f.unlink()
+
+def mkdir(path: str, exist_ok: bool = False, parents: bool = True):
+    file = Path(path).resolve()
     file.mkdir(exist_ok=exist_ok, parents=parents)
 
+def rmdir(path: str):
+    d = Path(path)
+    if d.exists():
+        shutil.rmtree(path)
+
+def lsdir(path: str):
+    d = Path(path)
+    if d.exists():
+        for item in d.iterdir():
+            print(item)
+
+def scan(path: str, filter: str = "*"):
+    d = Path(path)
+    if d.exists():
+        for item in d.rglob(filter):
+            print(item)
+
 if __name__ == "__main__":
+    metadata = {
+        "destination": "data/development/"
+    }
     while True: # Test command line
         cmd = input(" >>> ").replace(" ", "").lower()
         if cmd == "":
@@ -26,11 +56,25 @@ if __name__ == "__main__":
             break
         elif cmd == "clear":
             os.system("clear")
-        elif cmd == "touch":
+        elif cmd == "mkfile":
             file = input(" File : ")
-            touch("data/development/" + file)
+            mkfile(join(appdir(), metadata["destination"], file))
+        elif cmd == "rmfile":
+            file = input(" File : ")
+            rmfile(join(appdir(), metadata["destination"], file))
         elif cmd == "mkdir":
-            dir = input(" Directory : ")
-            mkdir("data/development/" + dir)
+            dirname = input(" Directory : ")
+            mkdir(join(appdir(), metadata["destination"], dirname))
+        elif cmd == "rmdir":
+            dirname = input(" Directory : ")
+            rmdir(join(appdir(), metadata["destination"], dirname))
+        elif cmd == "lsdir":
+            dirname = input(" Directory : ")
+            lsdir(join(appdir(), metadata["destination"], dirname))
+        elif cmd == "scan":
+            dirname = input(" Directory : ")
+            scan(join(appdir(), metadata["destination"], dirname))
+        elif cmd == "test":
+            pass
         else:
             print("Unknown : " + cmd)
